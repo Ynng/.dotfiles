aac54c13b84d38491fefa957ca33c63041134d98
Ynng <44907675+Ynng@users.noreply.github.com>
2021-09-10 23:52:41 -0400

    Improved setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,4 @@
-
-OHMYZSH=~/.oh-my-zsh
-OHMYZSHBACKUP=~/.oh-my-zsh-bak
-if test -d "$OHMYZSH"; then
-	echo "$OHMYZSH already exist, backing it up to $OHMYZSHBACKUP"
-	mv "$OHMYZSH" "$OHMYZSHBACKUP"
-fi
-# rm -r /home/wenqi/.oh-my-zsh
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
+#Git stuff
 GITCONFIG=~/.gitconfig
 GITCONFIGBACKUP=~/.gitconfig_bak
 if test -f "$GITCONFIG"; then
@@ -21,7 +6,9 @@ if test -f "$GITCONFIG"; then
 	mv "$GITCONFIG" "$GITCONFIGBACKUP"
 fi
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+git config credential.helper store
 
+#ZSHRC
 ZSHRC=~/.zshrc
 ZSHRCBACKUP=~/.zshrc_bak
 if test -f "$ZSHRC"; then
@@ -30,7 +17,27 @@ if test -f "$ZSHRC"; then
 fi
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
-p10k configure
+#OHMYZSH
+OHMYZSH=~/.oh-my-zsh
+OHMYZSHBACKUP=~/.oh-my-zsh-bak
+if test -d "$OHMYZSH"; then
+	echo "$OHMYZSH already exist, backing it up to $OHMYZSHBACKUP"
+	mv "$OHMYZSH" "$OHMYZSHBACKUP"
+fi
+# rm -r /home/wenqi/.oh-my-zsh
 
-#Git stuff
-git config credential.helper store
+#Download and installing oh-my-zsh
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+RUNZSH='no' KEEP_ZSHRC='yes' sh install.sh
+# sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#Downloading themes and plugins
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Setting up p10k
+# p10k configure
+
