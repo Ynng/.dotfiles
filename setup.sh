033250c7e7e0798cb5660f82a9ea2e13ec0f2f20
Ynng <44907675+Ynng@users.noreply.github.com>
2022-02-16 18:30:45 -0500

    refactored setup script

--- setup.sh
+++ setup.sh
@@ -1,47 +1,102 @@
-#Git stuff
-GITCONFIG=~/.gitconfig
-GITCONFIGBACKUP=~/.gitconfig_bak
-if test -f "$GITCONFIG"; then
-	echo "$GITCONFIG already exist, backing it up to $GITCONFIGBACKUP"
-	mv "$GITCONFIG" "$GITCONFIGBACKUP"
+#!/usr/bin/env bash
+
+echo "Setting up..."
+
+HOMEDIR=$HOME
+CURRENTDIR=$(pwd)
+
+# asks if user wants to install software
+echo "Do you want to install software? (y/n)"
+read -r answer
+
+#####################################
+#  _____          _        _ _     
+# |_   _|        | |      | | |    
+#   | | _ __  ___| |_ __ _| | |___ 
+#   | || '_ \/ __| __/ _` | | / __|
+#  _| || | | \__ \ || (_| | | \__ \
+#  \___/_| |_|___/\__\__,_|_|_|___/
+#####################################
+
+# Only do this if we have apt-get and user agrees
+if [ -x "$(command -v apt-get)" ] && [ "$answer" == "y" ]; then
+    # installs software
+    echo "Installing software from pacakges.txt..."
+    sudo apt-get update
+    xargs sudo apt-get install < $CURRENTDIR/packages.txt
 fi
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+
+#####################################
+#  _   _      _                     
+# | | | |    | |                    
+# | |_| | ___| |_ __   ___ _ __ ___ 
+# |  _  |/ _ \ | '_ \ / _ \ '__/ __|
+# | | | |  __/ | |_) |  __/ |  \__ \
+# \_| |_/\___|_| .__/ \___|_|  |___/
+#              | |                  
+#              |_|                  
+#####################################
+
+# backs up a file with time stamp
+backup() {
+    if [ -f $1 ]; then
+        echo "$1 already exists, backing up..."
+        mv $1 $1.bak.$(date +%s)
+    fi
+}
+
+# backs up a directory with time stamp
+backup_dir() {
+    if [ -d $1 ]; then
+        echo "$1 already exists, backing up..."
+        mv $1 $1.bak.$(date +%s)
+    fi
+}
+
+# symlinks a file and backs up the original
+symlink_and_backup() {
+    backup $1
+    ln -s $2 $1
+    echo "Symlinked $1 to $2"
+}
+
+#####################################
+#  _     _       _        
+# | |   (_)     | |       
+# | |    _ _ __ | | _____ 
+# | |   | | '_ \| |/ / __|
+# | |___| | | | |   <\__ \
+# \_____/_|_| |_|_|\_\___/
+#####################################
+
+#Git stuff
+echo "linking git config..."
+symlink_and_backup $HOMEDIR/.gitconfig $CURRENTDIR/.gitconfig
 
 #VIM
-VIM=~/.vimrc
-VIMBACKUP=~/.vimrc_bak
-if test -f "$VIM"; then
-	echo "$VIM already exist, backing it up to $VIMBACKUP"
-	mv "$VIM" "$VIMBACKUP"
-fi
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
+echo "linking vimrc..."
+symlink_and_backup $HOMEDIR/.vimrc $CURRENTDIR/.vimrc
 
 #P10K
-P10K=~/.p10k.zsh
-P10KBACKUP=~/.p10k.zsh-bak
-if test -f "$P10K"; then
-	echo "$P10K already exist, backing it up to $P10KBACKUP"
-	mv "$P10K" "$P10KBACKUP"
-fi
-ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+echo "linking p10k.zsh..."
+symlink_and_backup $HOMEDIR/.p10k.zsh $CURRENTDIR/.p10k.zsh
 
 #ZSHRC
-ZSHRC=~/.zshrc
-ZSHRCBACKUP=~/.zshrc_bak
-if test -f "$ZSHRC"; then
-	echo "$ZSHRC already exist, backing it up to $ZSHRCBACKUP"
-	mv "$ZSHRC" "$ZSHRCBACKUP"
-fi
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
+echo "linking zshrc..."
+symlink_and_backup $HOMEDIR/.zshrc $CURRENTDIR/.zshrc
 
 #OHMYZSH
-OHMYZSH=~/.oh-my-zsh
-OHMYZSHBACKUP=~/.oh-my-zsh-bak
-if test -d "$OHMYZSH"; then
-	echo "$OHMYZSH already exist, backing it up to $OHMYZSHBACKUP"
-	mv "$OHMYZSH" "$OHMYZSHBACKUP"
-fi
-# rm -r /home/wenqi/.oh-my-zsh
+echo "backing up oh-my-zsh directory..."
+backup_dir $HOMEDIR/.oh-my-zsh
+
+#####################################
+#          _     
+#         | |    
+#  _______| |__  
+# |_  / __| '_ \ 
+#  / /\__ \ | | |
+# /___|___/_| |_|
+#####################################
 
 #Download and installing oh-my-zsh
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
@@ -58,3 +113,15 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 # Setting up p10k
 # p10k configure
 
+#####################################
+# ___  ____          
+# |  \/  (_)         
+# | .  . |_ ___  ___ 
+# | |\/| | / __|/ __|
+# | |  | | \__ \ (__ 
+# \_|  |_/_|___/\___|
+#####################################
+
+# Installs nvm
+echo "Installing nvm..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
